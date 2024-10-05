94932d5e0b6da767c1b0c3c3b947d0f4c17fdf6e
Каримов Рустам <timrus12@gmail.com>
2024-10-05 09:04:22 +0300

    год високосный

---
+++ time.pas
@@ -0,0 +1,15 @@
+﻿begin
+  var year := readinteger('Введите год');
+
+    if (year mod 100) = 0 then
+    begin
+      print('Не високосный');
+      exit;
+    end;
+    if (year mod 400) = 0 then
+    begin
+      print('Не високосный');
+      exit;
+    end;
+  if (year mod 4)= 0 then print('Високосный')
+end.
