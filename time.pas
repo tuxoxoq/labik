31f60a494b58a8a8b87b9d30abd0f085234b7a4b
Каримов Рустам <timrus12@gmail.com>
2024-10-05 09:18:11 +0300

    время

--- time.pas
+++ time.pas
@@ -3,14 +3,14 @@
 
     if (year mod 100) = 0 then
     begin
-      print('Не високосный');
+      print('Не високосный, 365 дней');
       exit;
     end;
     if (year mod 400) = 0 then
     begin
-      print('Не високосный');
+      print('Не високосный, 365 дней');
       exit;
     end;
-  if (year mod 4)= 0 then print('Високосный')
-  else print('не високосный');
+  if (year mod 4)= 0 then print('Високосный, 366 дней')
+  else print('не високосный, 365 дней');
 end.
---
+++ timer.pas
@@ -0,0 +1,6 @@
+﻿begin
+  var n := readinteger('Введите количество часов');
+  var a := readinteger('Введите количество минут');
+  var b := readinteger('Введите количество секунд');
+  print(n * 3600 + a * 60 + b);
+end.
