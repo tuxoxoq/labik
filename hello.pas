6f4b42465840175a65c76d713329d0bdcbe578f1
Каримов Рустам <timrus12@gmail.com>
2024-10-05 08:43:39 +0300

    Ваше сообщение

--- hello.pas
+++ hello.pas
@@ -1,3 +1,12 @@
 ﻿begin
-  print('Привет ,мир!');
+  var n:=readinteger('Введите время:');
+  var time:string;
+  Assert(n in 0..23);
+  case n of
+  4..10:time:='Доброе утро, мир!';
+  11..16:time:='Добрый день, мир!';
+  17..22:time:='Доброй вечер, мир!';
+  else time:='Доброй ночи, мир!';
+  end;
+print(time);
 end.
