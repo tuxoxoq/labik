69cff963712ea91a9e6b2c79a588f53073dec4de
tuxoxoq <timrus12@gmail.com>
2024-10-12 22:58:58 +0300

    Приветствия

--- hello.pas
+++ hello.pas
@@ -1,12 +1,19 @@
-﻿begin
-  var n:=readinteger('Введите время:');
-  var time:string;
+begin
+  var priv := readstring('На каком языке вы разговариваете?');
+  if priv = 'Русский' then
+    println('Привет!');
+  if priv = 'Английский' then
+    println('Hi!');
+  if priv = 'Китайский' then
+    println('你好');
+  var n := readinteger('Введите время:');
+  var time: string;
   Assert(n in 0..23);
   case n of
-  4..10:time:='Доброе утро, мир!';
-  11..16:time:='Добрый день, мир!';
-  17..22:time:='Доброй вечер, мир!';
-  else time:='Доброй ночи, мир!';
+    4..10: time := 'Доброе утро, мир!';
+    11..16: time := 'Добрый день, мир!';
+    17..22: time := 'Доброй вечер, мир!';
+  else time := 'Доброй ночи, мир!';
   end;
-print(time);
+  print(time);
 end.
